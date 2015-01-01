90bc0d51b5d435e0ce6f5d5eca9de2a4c4fe755f
chanleong <lchshkg@gmail.com>
2015-01-01 23:33:50 +0800

    xml parser

---
+++ server/python/p.py
@@ -0,0 +1,53 @@
+#! /usr/bin/python
+
+class preprocessor:
+	@staticmethod
+	def do(file):
+		import re
+		f = open(file, "r")
+		b = "" 
+		for l in f:
+			if re.search("<jtis_journey_list", l):
+				b += "<jtis_journey_list>"
+			else:
+				b += l + "\n"
+		f.close()
+		return b
+
+class route:
+	def __init__(self, to, fr):
+		self._to = to
+		self._fr = fr
+		self._delim = "-"
+
+	def name(self):
+		return self._to + self._delim + self._fr
+
+	def __eq__(self, other):
+		return self.name() == other.name()
+
+	def __hash__(self):
+		return int(''.join(str(ord(c)) for c in self.name()))
+
+class parser:
+	def __init__(self, str):
+		import xml.etree.ElementTree as et
+		self._xmlRoot = et.fromstring(str)
+		self.data = {}
+		self.buildDict()
+
+	def buildDict(self):
+		for i in self._xmlRoot:
+			r = route(i.find("LOCATION_ID").text,i.find("DESTINATION_ID").text)
+			datum = {}
+			for j in i:
+				datum[j.tag] = j.text
+			self.data[r] = datum
+		return self.data
+	
+	def getColour(self, r):
+		b = self.data[r]
+		return b["COLOUR_ID"]
+
+pa = parser(preprocessor.do("journeytime.xml"))
+print pa.getColour(route("H11", "CH"))
